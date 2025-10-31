a6f34f40e0de317ab60701df510d3ba6f0e5803c
gulpanjul <gulpa.andhikac@gmail.com>
2025-10-31 08:20:42 +0700

    fix: cors origin error

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsAccess = cors({
-  origin: ["http://localhost:5173, https://gulpanjul-sistem-approval-cuti.vercel.app"],
+  origin: ["http://localhost:5173", "https://gulpanjul-sistem-approval-cuti.vercel.app"],
   credentials: true,
 });
 

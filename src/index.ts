978c573b17006765af6fe23699806eb3cc0e0cde
gulpanjul <gulpa.andhikac@gmail.com>
2025-10-31 08:14:57 +0700

    feat: setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
-import cors from "cors";
 import authRoutes from "./routes/auth.route";
 import cutiRoutes from "./routes/cuti.route";
+import corsAccess from "./middleware/cors";
 
 const app = express();
-app.use(cors());
+app.use(corsAccess);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cuti", cutiRoutes);
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server on ${PORT}`));
---
+++ src/middleware/cors.ts
@@ -0,0 +1,8 @@
+import cors from "cors";
+
+const corsAccess = cors({
+  origin: ["http://localhost:5173, https://gulpanjul-sistem-approval-cuti.vercel.app"],
+  credentials: true,
+});
+
+export default corsAccess;
